ecd69e7036572957f24dc2e6fa4e33d29f5e9e1e
Rapine Romaric <70980141+romaricrapine@users.noreply.github.com>
2020-11-03 15:26:13 +0100

    modif pdo

--- assets/pdo.php
+++ assets/pdo.php
@@ -2,8 +2,10 @@
 
 try {
     // Connexion à la base
-    $pdo = new PDO('mysql:host=localhost;dbname=projet-explorer', 'root', '');
-    $pdo->exec('SET NAMES "UTF8"');
+    $pdo = new PDO('mysql:host=localhost;dbname=projet-explorer;host=127.0.0.1', 'root', '', [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    ]);
+
 } catch (PDOException $e) {
     echo 'Erreur : ' . $e->getMessage();
     die();
