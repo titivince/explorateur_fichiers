f97062b24bc2d626716f0f76265ca0af0d238886
titivince <72084738+titivince@users.noreply.github.com>
2020-11-03 10:32:13 +0100

    add dbname

--- assets/pdo.php
+++ assets/pdo.php
@@ -2,7 +2,7 @@
 
 try {
     // Connexion à la base
-    $pdo = new PDO('mysql:host=localhost;dbname= ', 'root', '');
+    $pdo = new PDO('mysql:host=localhost;dbname=projet-explorer ', 'root', '');
     $pdo->exec('SET NAMES "UTF8"');
 } catch (PDOException $e) {
     echo 'Erreur : ' . $e->getMessage();
