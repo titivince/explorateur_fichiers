2877017bd4665bf740b72fb559471fbdcf23f007
titivince <72084738+titivince@users.noreply.github.com>
2020-11-04 13:37:24 +0100

    fix error

--- src/login.php
+++ src/login.php
@@ -11,16 +11,18 @@ $req = $pdo->prepare('SELECT * FROM user');
 $req->execute();
 
 $resultat = $req->fetchAll();
-
-foreach ($resultat as $list ){
-    if($_GET['pseudo'] == $list['pseudo'] && $_GET['password'] == $list['password']){
+if (isset($_GET['pseudo'])) {
+    foreach ($resultat as $list) {
+        if ($_GET['pseudo'] == $list['pseudo'] && $_GET['password'] == $list['password']) {
 
             session_start();
-            $_SESSION['id'] = $resultat['id'];
+            $_SESSION['id'] = $list['id'];
+            $_SESSION['pseudo'] = $list['pseudo'];
 
-            echo 'Bienvenue ';
-    }else {
-        echo'blabla';
+            $valid = 'Bienvenue '.$_SESSION['pseudo'];
+        } else {
+            $valid = 'mauvais pseudo ou mot de passe ';
+        }
     }
 }
 
@@ -28,30 +30,32 @@ foreach ($resultat as $list ){
 
 <!DOCTYPE html>
 <html lang="FR-fr">
-<head>
-    <meta charset="utf-8">
-    <title>Connexion</title>
-    <link rel="stylesheet" href="../assets/style.css">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css">
-</head>
-<body>
+    <head>
+        <meta charset="utf-8">
+        <title>Connexion</title>
+        <link rel="stylesheet" href="../assets/style.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css">
+    </head>
+    <body>
 
-<form class="box" method="GET">
+        <form class="box" method="GET">
 
-    <!-- Crée un compte Admin + User et indiquer ici les logs -->
-    <!-- <p>Admin : admin + admin <br>//<br> User : user + user</p>-->
-    <p>Commentaire dans le code sources à modifier !</p>
+            <!-- Crée un compte Admin + User et indiquer ici les logs -->
+            <!-- <p>Admin : admin + admin <br>//<br> User : user + user</p>-->
+            <p>Commentaire dans le code sources à modifier !</p>
 
-    <h1>Connexion</h1>
-    <!-- Changer le name pour la BDD -->
-    <input type="text" name="pseudo" placeholder="Pseudo">
-    <input type="password" name="password" placeholder="Mot de passe">
-    <input type="submit" value="Connexion">
+            <h1>Connexion</h1>
+            <!-- Changer le name pour la BDD -->
+            <input type="text" name="pseudo" placeholder="Pseudo">
+            <input type="password" name="password" placeholder="Mot de passe">
+            <input type="submit" value="Connexion">
 
-    <a class="signup" href="inscription.php" >Crée un compte</a>
+            <a class="signup" href="inscription.php">Crée un compte</a>
 
-</form>
-
-</body>
+        </form>
+        <?php if(isset($valid)){
+            echo $valid;
+        }  ?>
+    </body>
 </html>
