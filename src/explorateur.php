edf4966e315e6024e41edc8640870abf43c451ef
Rapine Romaric <70980141+romaricrapine@users.noreply.github.com>
2020-11-04 12:07:26 +0100

    explorateur 70%

--- src/explorateur.php
+++ src/explorateur.php
@@ -15,9 +15,11 @@ $user = $query->fetchAll();
 <!DOCTYPE html>
 <html lang="FR-fr">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
     <title>Explorateur de fichier</title>
+    <link rel="stylesheet" href="../assets/style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css">
 </head>
 <body>
 
