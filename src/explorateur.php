f16eccfeb81df018fab8a58b64f6860d53af284d
Rapine Romaric <70980141+romaricrapine@users.noreply.github.com>
2020-11-05 11:02:10 +0100

    modif explorateur + form.php

--- src/explorateur.php
+++ src/explorateur.php
@@ -2,6 +2,7 @@
 
 require_once('Connection.php');
 require('../views/block/nav.php');
+require('../views/block/footer.php');
 
 use App\Connection;
 
@@ -23,34 +24,36 @@ $user = $query->fetchAll();
 </head>
 <body>
 
-<table class="table">
+    <div class="explorateur">
+        <table class="table">
 
-    <tr>
-        <th><span class="icon"><img src="../assets/icon/folder-48.png" alt=""></span>Nom du fichier</th>
-        <th><span class="icon"><img src="../assets/icon/collapse-arrow-48.png" alt=""></span>Taille du fichier</th>
-        <th><span class="icon"><img src="../assets/icon/folder-48.png" alt=""></span>Ajouter le</th>
-        <th><span class="icon"><img src="../assets/icon/folder-48.png" alt=""></span>Actions</th>
-    </tr>
-
-
-    <?php foreach ($user as $item): ?>
-
-        <div>
-            <tr>
-
-                <td><?php echo  $item["name"] ?></td>
-                <td><?php echo  $item["first_name"] ?></td>
-                <td><?php echo  $item["pseudo"] ?></td>
-                <td><a class="delete" href="delete.php?lign_delete=<?= intval($item['id']) ?>">Supprimer</a>
-                    <a  class="update" href="form_of_modif.php?ligne_update=<?= intval($item['id']) ?> ">modifier</a>
-                </td>
-
+            <tr class="table-header">
+                <th><span class="icon"><img src="../assets/icon/folder-48.png" alt=""></span>Nom du fichier</th>
+                <th><span class="icon"><img src="../assets/icon/collapse-arrow-48.png" alt=""></span>Taille du fichier</th>
+                <th><span class="icon"><img src="../assets/icon/folder-48.png" alt=""></span>Ajouter le</th>
+                <th><span class="icon"><img src="../assets/icon/folder-48.png" alt=""></span>Actions</th>
             </tr>
 
-        </div>
-    <?php endforeach ?>
 
-    <?php require('../views/block/footer.php'); ?>
-    
+            <?php foreach ($user as $item): ?>
+
+                <div>
+                    <tr>
+
+                        <td><?php echo  $item["name"] ?></td>
+                        <td><?php echo  $item["first_name"] ?></td>
+                        <td><?php echo  $item["pseudo"] ?></td>
+                        <td><a class="delete" href="delete.php?lign_delete=<?= intval($item['id']) ?>">Supprimer</a>
+                            <a  class="update" href="form_of_modif.php?ligne_update=<?= intval($item['id']) ?> ">modifier</a>
+                        </td>
+
+                    </tr>
+
+                </div>
+            <?php endforeach ?>
+        </table>
+    </div>
+
+
 </body>
 </html>
--- src/form.php
+++ src/form.php
@@ -1,5 +1,8 @@
 <?php
+
 require_once('Connection.php');
+require('../views/block/nav.php');
+require('../views/block/footer.php');
 
 use App\Connection;
 
@@ -19,3 +22,22 @@ $pdo = (new Connection())->getPdo();
 echo "votre compte a bien été créer , vous aller être redirigé";
 
 header('refresh:5; url:home.php');
+
+?>
+
+<!doctype html>
+<html lang="FR-fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <META http-equiv="refresh" content="3; URL=http://localhost/explorateur_fichiers/src/explorateur.php">
+    <link rel="stylesheet" href="../assets/style.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css">
+    <title>Connexion réussite</title>
+</head>
+<body>
+
+</body>
+</html>
